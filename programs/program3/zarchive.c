c823ab0b784a951d12620b7bc0ec8c1925a98c81
d-man7562 <domenic.mancuso@maine.edu>
2025-03-02 22:42:53 -0500

    program 3 work more
    Signed-off-by: d-man7562 <domenic.mancuso@maine.edu>

--- programs/program3/zarchive.c
+++ programs/program3/zarchive.c
@@ -2,57 +2,123 @@
 #include <pwd.h>
 #include <unistd.h>
 #include <dirent.h>
-void archive(char* fn, char * archivename);
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdbool.h>
+void archive(char* fn);
 void unarchive(char* fn);
-
+char * give_proper_name(char * name);
+bool file_exists_in_current_dir(const char *filename);
 int main(int argc, char**argv) {
-
+/*should be argv[2]*/
 char* archivename;
 
-if (argv[3] !=NULL) {
-archivename = argv[3];
-}
-
-/*Check that first argument is a character, x or c. If not,
-throw an error. */
-/*archive*/
-if (strcmp(argv[1],"c") == 0) {
-archive(argv[2], archivename);
-}
 /*get users name from system*/
 uid_t uid = getuid();
-struct passwd pw = getpwuid(uid);
-/*extract*/
-if (strcmp(argv[1],"x") == 0) {
-unarchive(argv[2]);
+struct passwd *pw = getpwuid(uid);
+
+if (argc ==2) {
+    
+    if (argv[1] == 'c'){
+        printf("Archived file name has not been provided. Please enter name of archived file:\n");
+        archivename = scanf(stdin);
+        give_proper_name(archivename);
+        archive(archivename);
+    }
+    
+    if (argv[1] == 'x'){
+
+        unarchive(/*filename*/);
+    }
+
+
+
 }
-else{
-perror("Usage: zarchive [x,c] filename");
+if (argc ==3) {
+
+archivename = argv[2];
+give_proper_name(archivename);
+if (argv[1] == 'c'){
+    archive(archivename);
+}
+    if (argv[1] == 'x'){
+        unarchive(/*filename*/);
+    }
 }
 
+else{
+    fprintf(stderr, "Usage: zarchive [x,c] filename\n");
+    return 1;
+}
 return 0;
 }
 
-void archive(char * fn,char * archivename) { 
-/*check for missing filename*/
-if (fn == NULL){
-perror("Error: missing filename");
+char * give_proper_name(char * name){
+    int len = strlen(name);
+    if (!len >= 2 && name[len-2] == '.' && name[len-1] == 'z') {
+        char * new_string = '.z'; 
+        name = strcat(new_string, name);
+    }
+    return name;
+    }
+
+    bool file_exists_in_current_dir(const char *filename) {
+        DIR *dir;
+        struct dirent *entry;
+        bool found = false;
+        
+        // Open the current directory
+        dir = opendir(".");
+        if (dir == NULL) {
+            perror("Unable to open current directory");
+            return false;
+        }
+        
+        // Read directory entries
+        while ((entry = readdir(dir)) != NULL) {
+            // Compare the entry name with the target filename
+            if (strcmp(entry->d_name, filename) == 0) {
+                found = true;
+                break;
+            }
+        }
+        
+        // Close the directory
+        closedir(dir);
+        
+        return found;
+    }
+void archive(char * fn) { 
+/*check if file name is in the system
+if so, return an error. 
+otherwise, continue with control flow*/
+if (file_exists_in_current_dir(fn)) {
+perror("Error: file already exists in current directory.\n");
 return 1;
 }
-if (archivename == NULL)
-printf("Archived file name has not been provided. Please enter name of archived file:\n");
-archivename = scanf(stdin);
-size_t len = strlen(filename);
-if (len >= 2 && filename[len-2] == '.' && filename[len-1] == 'z') {
-}
+DIR *dir;
+        struct dirent *entry;
+        dir = opendir(".");
+        while ((entry = readdir(dir)) != NULL) {
+            snprintf(filepath, sizeof(filepath), "./%s", entry->d_name);
+        
+        // Get file statistics
+        if (stat(filepath, &file_stat) == -1) {
+            perror("Failed to get file stats");
+            continue;
+        }
+        
+        // Format the modification time
+        struct tm *time_info = localtime(&file_stat.st_mtime);
+        strftime(timestr, sizeof(timestr), "%Y-%m-%d %H:%M:%S", time_info);
+        
+        // Print file information
+        printf("%-30s %-10ld %-20s\n", 
+               entry->d_name, 
+               file_stat.st_size, 
+               timestr);
+    }
 
 }
-
 
  
-
-
-/* read file to archive
-check if argv3 is null
-archivename is arg3
-
