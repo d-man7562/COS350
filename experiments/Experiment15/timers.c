2c7876517f56f027d4560bc431e0df0aa96c4a35
d-man7562 <domenic.mancuso@maine.edu>
2025-03-26 20:52:43 -0400

    experiment 15
    Signed-off-by: d-man7562 <domenic.mancuso@maine.edu>

--- experiments/Experiment15/timers.c
+++ experiments/Experiment15/timers.c
@@ -17,6 +17,7 @@ int main(){
 struct itimerval timer;
 struct sigaction sa;
 sa.sa_handler = &rtimer_handler;
+sa.sa_flags = SA_RESTART;
 timer.it_value.tv_sec = 0;
 timer.it_interval.tv_sec = 0;
 timer.it_value.tv_usec = 1000;
@@ -24,6 +25,9 @@ timer.it_interval.tv_usec = 1000;
 sigaction(SIGALRM, &sa, NULL);
 
 setitimer(ITIMER_REAL, &timer, NULL);
+getitimer(ITIMER_REAL,&timer);
+while (1){}
 
+	return 0;
 }
 
