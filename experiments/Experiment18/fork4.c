dbdcc551c6c66c0c53170fc57fe7fc9eb521e61b
d-man7562 <domenic.mancuso@maine.edu>
2025-04-02 20:16:47 -0400

    experiment 18
    Signed-off-by: d-man7562 <domenic.mancuso@maine.edu>

---
+++ experiments/Experiment18/fork4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+int main(){
+
+printf("count: 1 -- pid:%d \n", getpid());
+int parent_pid = getpid();
+fork();
+
+for(int i=2; i<6; i++){
+if (getpid()!= parent_pid){
+printf("count: %d -- pid: %d\n",i,getpid());
+/* Linked List sort of thing going on here */
+parent_pid = getpid();
+fork();
+
+}
+}
+
+}
